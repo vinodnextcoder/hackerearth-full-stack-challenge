6569145249492ffe56d16e2ee06621f1a19a7884
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-08-30 14:06:41 +0530

    bill schema

---
+++ bill-backend/src/modules/bill/dtos/user.create.dto.ts
@@ -0,0 +1,20 @@
+import { Type } from 'class-transformer';
+import {
+    IsNotEmpty
+} from 'class-validator';
+
+
+export class BillCreateDto {
+
+
+    @IsNotEmpty()
+    readonly billStatus: string;
+
+    
+    @Type(() => Date)
+    readonly billDate: Date;
+
+
+    @IsNotEmpty()
+    readonly unitConsume: string;
+}
